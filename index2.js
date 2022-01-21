3f08c67b59b36c14c49f4a44480011c35cb74a71
Brandon <brandon.dahlberg21@gmail.com>
2022-01-21 23:19:40 +0000

    made the app again using promises

--- index2.js
+++ index2.js
@@ -1,1 +1,13 @@
+const {nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation() 
+.then((passTimes) => {
+  console.log(passTimes);
+})
+.catch((error) => {
+  console.log('It didnt work: ', error.message);
+})
+
+
+  
 
